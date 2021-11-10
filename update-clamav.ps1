9a9d84acd1c1980eec0dfd77adb210b0db655460
HayZun <58841936+HayZun@users.noreply.github.com>
2021-11-10 16:18:53 +0100

    Update update-clamav.ps1

--- update-clamav.ps1
+++ update-clamav.ps1
@@ -1,4 +1,4 @@
-#rendre le script executable en tant qu'admin
+#make the script executable as admin
 param([switch]$Elevated)
 
 function Test-Admin {
@@ -9,7 +9,7 @@ function Test-Admin {
 if ((Test-Admin) -eq $false)  {
     if ($elevated) 
     {
-        #essayé d'élever
+        #tried to raise
     } 
     else {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
@@ -18,23 +18,23 @@ if ((Test-Admin) -eq $false)  {
 exit
 }
 
-#instance pour check internet
+#instance for check internet
 $connected = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}’)).IsConnectedToInternet
 
-#verifier si la machine a acces a internet
+#check if the machine has internet access
 if ($Connected -eq $True) {
 
-    #utilise TLS 1.2
+    #uses TLS 1.2
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-    #variable d'environnements
+    #variable environments
     $tempdest=(Get-Childitem -Path Env:TEMP).Value + "\ClamAV"
     $targetprogramfiles=$env:ProgramFiles
 
-    #Vérifier la présence des répertoires temp/clamav + programmes/clamav, si non, création des 2 répertoires
+    #Check the presence of the temporary file, if not, create the directory temp\clamav
     If ((Test-Path $tempdest) -eq $False) {New-Item $tempdest -itemType Directory}
 
-    #récupérer le cookie
+    #retrieve the cookie
     $Request = Invoke-WebRequest https://www.clamav.net/ -Method 'GET' -Headers @{
       "method"="GET"
       "authority"="www.clamav.net"
@@ -53,11 +53,11 @@ if ($Connected -eq $True) {
       "accept-language"="fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7"
     };
 
-    #parsing du cookie
+    #cookie parsing
     $cookie, $bin = $Request.Headers["Set-Cookie"] -split ";", 2
     $realcookie = "_ga=GA1.2.684229815.1629827059; _gid=GA1.2.657091150.1629827059; " + $cookie
 
-    #je récupère tous les HREF
+    #I get all the HREF
     $r=iwr https://www.clamav.net/downloads#otherversions -UseBasicParsing -Headers @{
       "method"="GET"
       "authority"="www.clamav.net"
@@ -76,18 +76,18 @@ if ($Connected -eq $True) {
       "cookie"=$realcookie
       }
 
-    #je filtre en récupérant le href avec comme extension ".zip"
+    #I filter by retrieving the href with the extension ".zip".
     $href=($r.Links |?{$_.href -match "(win.x64.zip$)"}).href[0]
 
-    #je forme le lien de dl la dernière version pour windows 64 bit
+    #I form the link of dl the last version for windows 64 bit
     $links="https://www.clamav.net" + $href
 
-    #créaton du fichier.txt de actualversion s'il n'est pas déjà crée (1ere fois qu'il s'installe)
+    #create the .txt file of actualversion if it is not already created (1st time it is installed)
     If ((Test-Path "$tempdest\actualversion.txt") -eq $False) {
     
-        #installation clamav
+        #clamav installation
 
-        #request pour DL le .zip
+        #request for DL the .zip
         $Response=Invoke-WebRequest -Uri $links -Headers @{
         "method"="GET"
           "authority"="www.clamav.net"
@@ -111,26 +111,26 @@ if ($Connected -eq $True) {
         #unzip
         Expand-Archive -LiteralPath "$tempdest\ClamAV.zip" -DestinationPath "$tempdest\" 
 
-        #connaître le nom de l'archive
+        #know the name of the archive
         $name = (dir $tempdest).Name -match "win"
 
         #rename
         Rename-Item -Path "$tempdest\$name" -NewName "$tempdest\ClamAV"
 
-        #copie de l'archive
+        #copy of the archive
         Copy-Item –Path "$tempdest\ClamAV" –Destination $targetprogramfiles -Force -Recurse
 
-        #création d'un fichier.txt le lien utilisé pour DL
+        #creation of a .txt file the link used for DL
         New-Item "$tempdest\actualversion.txt" -type file
 
-        #écriture dans le fichier
+        #writing to the file
         ADD-content -path "$tempdest\actualversion.txt" -value $links
 
-        #actions_supplémntaires, écriture des .conf
+        #additional_actions, writing .conf
         Copy-Item -PAth "$targetprogramfiles\ClamAV\conf_examples\freshclam.conf.sample" -Destination "$targetprogramfiles\ClamAV\freshclam.conf" -Force -Recurse
         Copy-Item -PAth "$targetprogramfiles\ClamAV\conf_examples\clamd.conf.sample" -Destination "$targetprogramfiles\ClamAV\clamd.conf" -Force -Recurse
 
-        #remplacez le "Example" de chaque .conf par "#Example"
+        #replace the "Example" in each .conf with "#Example
         $Filefresh =  "$targetprogramfiles\ClamAV\freshclam.conf"
         $Fileclamd =  "$targetprogramfiles\ClamAV\clamd.conf"
         $content = Get-Content $Filefresh | foreach { $_ -replace "Example","#Example" }
@@ -141,24 +141,24 @@ if ($Connected -eq $True) {
         #execute freshclam.exe
         start-process -FilePath "$targetprogramfiles\ClamAV\freshclam.exe" -Verb RunAs
 
-        #supression des actuelles fichiers pour gagner du stockage
+        #deleting current files to save storage space
         Remove-Item "$tempdest\clamav.zip"
         Remove-Item "$tempdest\ClamAV" -Recurse
         }
 
-    #si ce n'est pas la 1ère install
+    #if it is not the 1st installation
     If ((Test-Path "$tempdest\actualversion.txt") -eq $True) {
     
         $actualversion=Get-Content -Path "$tempdest\actualversion.txt"
         If (($actualversion -eq $links) -eq $False){
 
-            #suppression des anciens fichiers de clamav
+            #deleting old clamav files
             If ((Test-Path "$targetprogramfiles\ClamAV") -eq $True) {Remove-Item "$targetprogramfiles\ClamAV" -Recurse}
 
-            #suppression actualversion
+            #deleting actualversion
             Remove-Item "$tempdest\actualversion.txt" -Recurse
 
-            #request pour DL le .zip
+            #request for DL the .zip
             $Response=Invoke-WebRequest -Uri $links -Headers @{
             "method"="GET"
               "authority"="www.clamav.net"
@@ -182,26 +182,26 @@ if ($Connected -eq $True) {
             #unzip
             Expand-Archive -LiteralPath "$tempdest\ClamAV.zip" -DestinationPath "$tempdest\" 
 
-            #connaître le nom de l'archive
+            #know the name of the archive
             $name = (dir $tempdest).Name -match "win"
 
             #rename
             Rename-Item -Path "$tempdest\$name" -NewName "$tempdest\ClamAV"
 
-            #copie de l'archive
+            #copy of the archive
             Copy-Item –Path "$tempdest\ClamAV" –Destination $targetprogramfiles -Force -Recurse
 
-            #création d'un fichier.txt le lien utilisé pour DL
+            #creation of a .txt file the link used for DL
             New-Item "$tempdest\actualversion.txt" -type file
 
-            #écriture dans le fichier
+            #writing to the file
             ADD-content -path "$tempdest\actualversion.txt" -value $links
 
-            #actions_supplémntaires, écriture des .conf
+            #additional_actions, writing .conf
             Copy-Item -PAth "$targetprogramfiles\ClamAV\conf_examples\freshclam.conf.sample" -Destination "$targetprogramfiles\ClamAV\freshclam.conf" -Force -Recurse
             Copy-Item -PAth "$targetprogramfiles\ClamAV\conf_examples\clamd.conf.sample" -Destination "$targetprogramfiles\ClamAV\clamd.conf" -Force -Recurse
 
-            #remplacez le "Example" de chaque .conf par "#Example"
+            #replace the "Example" in each .conf with "#Example
             $Filefresh =  "$targetprogramfiles\ClamAV\freshclam.conf"
             $Fileclamd =  "$targetprogramfiles\ClamAV\clamd.conf"
             $content = Get-Content $Filefresh | foreach { $_ -replace "Example","#Example" }
