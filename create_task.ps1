9e5d8a7ea89d714ed83f48c51e2ed8a0acbaaab2
HayZun <paul.durieux@humansconnexion.com>
2024-02-13 17:15:24 +0100

    update

--- create_task.ps1
+++ create_task.ps1
@@ -1,24 +1,28 @@
 <#
 ###################################################################
-Script Name : update-clamav.ps1                                                                                           
+Script Name : create_task.ps1                                                                                      
 Version : 1.0                                                                                      
 Author : Paul Durieux                                               
 Email : paul.durieux@data-expertise.com                                           
 ###################################################################
 #>
 
-#déplacer le script update-clamav.ps1 dans le répertoire C:\Scripts\
-Copy-Item -Path "./update-clamav.ps1" -Destination "C:\Scripts\update-clamav.ps1"
+#déplacer le script .\update-clamav.ps1 dans C:\Scripts
+Copy-Item -Path .\update-clamav.ps1 -Destination "C:\Scripts\update-clamav.ps1" -Force
 
 #créer le répertoire C:\temp\ClamAV
 if (!($(Test-Path "C:\temp\ClamAV"))) {
   mkdir "C:\temp\ClamAV" -ea 0
 }
 
-#create task on task scheduler at logon
+#create task on task scheduler at logon and éxécuter le script même si l'utilisateur n'est pas connecté 1 fois par jour à 8h
 $taskname = "ClamAV Update"
 $taskdescription = "Update ClamAV"
 $taskaction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "C:\Scripts\update-clamav.ps1"
-$tasktrigger = New-ScheduledTaskTrigger -AtLogon
+$tasktrigger = New-ScheduledTaskTrigge -AtLogOn -Daily -DaysInterval 1 -At 8am
 $tasksettings = New-ScheduledTaskSettingsSet -StartWhenAvailable
 Register-ScheduledTask -TaskName $taskname -Action $taskaction -Trigger $tasktrigger -Description $taskdescription -Settings $tasksettings
+
+
+#start the task now
+#Start-ScheduledTask -TaskName $taskname
