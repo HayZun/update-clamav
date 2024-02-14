23090f5870c217d0feb80fa6c80c6c991b210ebc
HayZun <paul.durieux@humansconnexion.com>
2024-02-14 10:16:34 +0100

    up

--- create_task.ps1
+++ create_task.ps1
@@ -7,10 +7,19 @@ Email : paul.durieux@data-expertise.com
 ###################################################################
 #>
 
-#déplacer le script .\update-clamav.ps1 dans C:\Scripts
-$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-$pathInstallTaskClamav = "C:\Users\$currentUser\Downloads\update-clamav-main\update-clamav.ps1"
+#Obtenir les droits d'administration
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+  Write-Warning "Vous n'avez pas les droits d'administration pour exécuter ce script !"
+  Write-Warning "Relancez ce script en tant qu'administrateur !"
+  Break
+}
 
+$currentUser = $env:USERNAME
+
+# Chemin du script update-clamav.ps1
+$pathInstallTaskClamav = "C:\Utilisateurs\$currentUser\Downloads\update-clamav-main\update-clamav.ps1"
+
+# déplacer le script .\update-clamav.ps1 dans C:\Scripts
 Copy-Item -Path $pathInstallTaskClamav -Destination "C:\Scripts\update-clamav.ps1" -Force
 
 #créer le répertoire C:\temp\ClamAV
@@ -24,7 +33,7 @@ $taskdescription = "Update ClamAV"
 $taskaction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "C:\Scripts\update-clamav.ps1"
 $tasktrigger = New-ScheduledTaskTrigger -Daily -At 8am
 $tasksettings = New-ScheduledTaskSettingsSet -StartWhenAvailable
-$taskprincipal = New-ScheduledTaskPrincipal -UserId "scanisette" -RunLevel Highest
+$taskprincipal = New-ScheduledTaskPrincipal -UserId $currentUser -RunLevel Highest
 
 Register-ScheduledTask -TaskName $taskname -Action $taskaction -Trigger $tasktrigger -Description $taskdescription -Settings $tasksettings -Principal $taskprincipal
 
