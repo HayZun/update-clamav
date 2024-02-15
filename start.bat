aab36a9de640c46f49c0f391aba5979a37f397c3
HayZun <paul.durieux@humansconnexion.com>
2024-02-15 12:09:41 +0100

    update

--- start.bat
+++ start.bat
@@ -1,8 +1,19 @@
+@REM @echo off
+@REM set "folder_path=C:\users\%USERNAME%\Downloads\update-clamav-main\update-clamav-main"
+
+@REM cmd /c powershell -Command Unblock-File -Path "%folder_path%\update-clamav.ps1"
+@REM cmd /c powershell -Command Unblock-File -Path "%folder_path%\create_task.ps1"
+
+
+
+@REM REM Run the script to create the task
+@REM REM powershell -Command "Start-Process -Verb RunAs -FilePath \"%folder_path%\create_task.ps1\""
+
 @echo off
 set "folder_path=C:\users\%USERNAME%\Downloads\update-clamav-main\update-clamav-main"
 
-REM Unblock the files
-forfiles /p "%folder_path%" /c "cmd /c powershell -Command Unblock-File -Path \"%~f0\"" 
-REM Run the script to create the task
-powershell -Command "Start-Process -Verb RunAs -FilePath \"%folder_path%\create_task.ps1\""
+for %%F in ("%folder_path%\*.ps1") do (
+    cmd /c powershell -Command "Unblock-File -Path \"%%F\""
+)
+
 ```
