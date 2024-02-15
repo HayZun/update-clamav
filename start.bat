267ac015179a3bed2d63192f3dfadfa2c4874e54
HayZun <paul.durieux@humansconnexion.com>
2024-02-15 15:28:29 +0100

    debug start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
-pause
+
 REM Unblock all files in the folder
 for %%F in ("%CD%\*.ps1") do (
     cmd /c powershell -Command "Unblock-File -Path \"%%F\""
 )
-pause
+
 REM Run the PowerShell script
 powershell -ExecutionPolicy Bypass -File "%CD%\create_task.ps1"
 
