645acf42110f1c1df75110afe04241bcb2185bfa
HayZun <paul.durieux@humansconnexion.com>
2024-02-15 15:22:06 +0100

    fix absolute path

--- start.bat
+++ start.bat
@@ -1,14 +1,11 @@
 @echo off
 
-REM Set the folder path
-set "folder_path=C:\users\%USERNAME%\Downloads\update-clamav-main\update-clamav-main"
-
 REM Unblock all files in the folder
-for %%F in ("%folder_path%\*.ps1") do (
+for %%F in ("%CD%\*.ps1") do (
     cmd /c powershell -Command "Unblock-File -Path \"%%F\""
 )
 
 REM Run the PowerShell script
-powershell -ExecutionPolicy Bypass -File "%folder_path%\create_task.ps1"
+powershell -ExecutionPolicy Bypass -File "%CD%\script.ps1"
 
 ```
